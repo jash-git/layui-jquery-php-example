5aa17889b79da5b5d0cdcbb7b5e778973e9c1681
jash.liao <jash.liao@gmail.com>
2023-12-04 09:45:37 +0800

    Add 202312040945

---
+++ 02/index_api.php
@@ -0,0 +1,35 @@
+﻿<?php
+    header('Content-Type: application/json; charset=utf-8');
+    
+    $jsonData = $_POST['JsonInput'];
+    $dataArray = json_decode($jsonData, true); 
+    
+    $data = array();
+    foreach ($dataArray as $obj) {
+        $objectBuf = new stdClass();
+        $objectBuf->name = $obj['name'];
+        $objectBuf->age = $obj['age'];
+        $data[] = $objectBuf;
+    }
+    $objectBuf01 = new stdClass();
+    $objectBuf01->name = "jash.liao";
+    $objectBuf01->age = "XX";
+    $data[] = $objectBuf01;
+    
+    /*寫死回傳回傳物件
+    $data = array();
+    $object1 = new stdClass();
+    $object1->name = "John Doe";
+    $object1->age = 30;
+    $data[] = $object1;
+
+    $object2 = new stdClass();
+    $object2->name = "Jane Doe";
+    $object2->age = 25;
+    $data[] = $object2;
+    */
+    
+    // Encode the array to JSON
+    $Strjson = json_encode($data);
+    echo $Strjson;
+?>
--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,3 @@
 ﻿00_基本目錄結構
-01_PHP物件轉JSON [php object array to jquery ajax]
-02_
+01_前端(JQuery Ajax)接收由後端(PHP)的JSON字串並透過前端(JS)語法還原成對應物件 [php object array to jquery ajax]
+02_後端(PHP)接收由前端(JQuery Ajax)的JSON字串並透過後端(PHP)語法還原成對應物件 [jquery ajax object array to php]
